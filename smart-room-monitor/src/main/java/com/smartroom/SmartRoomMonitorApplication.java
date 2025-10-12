7806b4c2ab6dac4edfa9fbeb3bafb3d076a146fc
Selin Zehra Bölükbaş <selinz.bolukbas@gmail.com>
2025-10-12 19:47:09 +0300

    proje yapılandırması

---
+++ smart-room-monitor/src/main/java/com/smartroom/SmartRoomMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.smartroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRoomMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartRoomMonitorApplication.class, args);
+	}
+
+}
